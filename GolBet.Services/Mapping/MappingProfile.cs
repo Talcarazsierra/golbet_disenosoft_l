dcf42d6ef9a9fc9687781df00098f4762ca05e02
Talcarazsierra <tana942008@hotmail.com>
2026-09-19 17:30:15 -0500

    Add match detail page and board filters (Module 5)

---
+++ GolBet.Services/Mapping/MappingProfile.cs
@@ -0,0 +1,21 @@
+﻿// GolBet.Services/Mapping/MappingProfile.cs 
+
+using AutoMapper;
+using GolBet.Entities;
+using GolBet.Services.DTOs;
+namespace GolBet.Services.Mapping;
+public class MappingProfile : Profile
+{
+    public MappingProfile()
+    {
+        // Flattening by convention: 
+        // MatchDto.HomeTeamName  <- Match.HomeTeam.Name 
+        // MatchDto.AwayTeamCrestUrl <- Match.AwayTeam.CrestUrl 
+        CreateMap<Match, MatchDto>();
+        // GolBet.Services/Mapping/MappingProfile.cs  (agregar dentro del constructor) 
+
+        CreateMap<Match, MatchDetailDto>()  //Destino
+            .ForMember(dto => dto.TotalBets,
+                       options => options.MapFrom(match => match.Bets.Count)); //Origen 
+    }
+}
